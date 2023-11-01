d2998675e61a1232fa5761a81e6f107f65241593
Awais Ali <58628571+Awais007811@users.noreply.github.com>
2023-11-02 00:02:17 +0500

    Assignment Show Name in React

---
+++ W2_D3/w2d3Assignment/app.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0);
+  const callName = ()=>{
+    alert("It's Awais Ali ");
+  }
+
+  return (
+    <>
+      <div>
+        {}
+        <button onClick={callName}>ClickMe</button>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
