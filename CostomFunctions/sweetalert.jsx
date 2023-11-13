645112cec5a033f80c126d184268735eb06ac9cd
Awais Ali <58628571+Awais007811@users.noreply.github.com>
2023-11-14 00:06:08 +0500

    SweetAlertAssignment

--- CostomFunctions/sweetalert.jsx
+++ CostomFunctions/sweetalert.jsx
@@ -13,25 +13,25 @@ function App() {
   return (
     <React.Fragment>
      <div  className=" flex flex-col justify-center items-center">
-      <h1 className=" text-lg font-bold m-5 center">Contact Form</h1>
+      <h1 className="text-lg font-bold m-5 center bg-black border-red-600 text-white w-100 p-5 rounded-xl ">Contact Form</h1>
       
         <form onSubmit={(e)=>{
           e.preventDefault();
           Swal.fire({
             title: 'Form submitted',
-            text: `name: ${name},email:${email},message:${message}`,
+            html: `name:${name}, email:${email}, message:${message}`,
             icon: 'success',
-            confirmButtonText: 'Cool'
+            confirmButtonText: 'Done'
           })
         }
         }
 
-         className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" >
+         className="shadow-md rounded px-8 pt-6 pb-8 mb-4 bg-gray-400 shadow-red-600" >
          <div>
          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
         Username
       </label>
-            <input onChange={(e) => {
+            <input value={name} onChange={(e) => {
               setName(e.target.value);
             }} type="text" placeholder="Enter Your Name" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required/>    
           </div>
@@ -39,13 +39,17 @@ function App() {
           <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
         Email
       </label>
-          <input type="email" placeholder="Enter Your Email" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required />
+          <input value={email} onChange={(e)=>{
+            setEmail(e.target.value);
+          }} type="email" placeholder="Enter Your Email" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required />
           </div> 
           <div>
           <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
         Website
       </label>
-          <input  type="text" placeholder="Enter Your Web URL" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required/>
+          <input value={message} onChange={(e)=>{
+            setMessage(e.target.value);
+          }} type="text" placeholder="Enter Your Web URL" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required/>
           </div>
           <div>
           <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
